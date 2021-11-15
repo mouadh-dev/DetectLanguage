00fc872e2c98c68d0e43dbce83c6f4df333af7a4
mouadh laabidi <mouadh.laa@gmail.com>
2021-11-15 21:16:32 +0100

    add git ignore

--- index.php
+++ index.php
@@ -9,11 +9,7 @@ for ($i=2;$i< count($argv);$i++){
 }
 
 $detector = new LanguageDetector\LanguageDetector();
-echo "hte language is ==>";
-echo $detector->evaluate($text);
-
-
-//echo $detector->evaluate($text);
+echo "the language is ==> ".$detector->evaluate($text);
 
 
 ?>
